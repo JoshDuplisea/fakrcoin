226a5eda1cc20b95be99caed538089f769fbf18a
JoshDuplisea <JoshuaS.Duplisea@gmail.com>
2018-01-30 22:17:01 -0400

    Modified Naivechain, and created NW.JS GUI
    Modified Naivechain, and created NW.JS GUI for 4 Native functions of original script. Needs major UI improvements, meaningful JSON -> HTML conversion, not just dumps. Tested on Linux w/NW.js v0.28.0 SDK.

---
+++ scripts.js
@@ -0,0 +1,71 @@
+/*
+Copyright 2018 Joshua Duplisea
+*/
+
+var logResults = (response) => {
+	let data = JSON.stringify(response);
+	console.log(data);		
+	document.getElementById("main").innerHTML = data.toString();
+}
+var blockExplorer = () => {
+	fetch('http://localhost:3001/blocks', {
+		method: 'get'
+	})
+	.then((res) => res.json())
+	.then(function(res){
+		logResults(res)
+	})
+	.catch(function(err) {
+		console.log(`Error: ${err}`);
+	});
+}
+var addPeer = () => {
+	fetch("http://localhost:3001/addPeer",
+	{
+		headers: {
+		  'Content-Type': 'application/json'
+		},
+		method: "POST",
+		body: JSON.stringify({"peer" : "ws://localhost:6001"})
+	})
+	.then((res) => res.json())
+	.then(function(res){
+		logResults(res)
+	})
+	.catch(function(err) {
+		console.log(`Error: ${err}`);
+	});
+}
+var activePeers = () => {
+	fetch('http://localhost:3001/peers', {
+		method: 'get'
+	})
+	.then((res) => res.json())
+	.then(function(res){
+		logResults(res)
+	})
+	.catch(function(err) {
+		console.log(`Error: ${err}`);
+	});
+}
+var mineBlocks = () => {
+	fetch("http://localhost:3001/mineBlock",
+	{
+		headers: {
+		  'Content-Type': 'application/json'
+		},
+		method: "POST",
+		body: JSON.stringify({"data" : "Data in blocks past Genesis block"})
+	})
+	.then((res) => res.json())
+	.then(function(res){
+		logResults(res)
+	})
+	.catch(function(err) {
+		console.log(`Error: ${err}`);
+	});
+}
+document.getElementById("block-explorer").addEventListener("click", blockExplorer);
+document.getElementById("add-peer").addEventListener("click", addPeer);
+document.getElementById("active-peers").addEventListener("click", activePeers);
+document.getElementById("mine-blocks").addEventListener("click", mineBlocks);
